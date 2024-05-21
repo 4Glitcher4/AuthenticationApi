a3dd83c77b93c4fe36de3f74618d10063ba8eaca
4Glitcher4 <n.black@gosu.kz>
2024-05-21 15:07:01 +0500

    Feat add profile controller

---
+++ AuthenticationApi/Controllers/ProfileController.cs
@@ -0,0 +1,55 @@
+﻿using AuthenticationApi.DataRepository.GenericRepository;
+using AuthenticationApi.DataRepository.Models;
+using AuthenticationApi.ModelsDto;
+using AuthenticationApi.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
+
+namespace AuthenticationApi.Controllers
+{
+    [ApiController]
+    [Route("Profile")]
+    [Authorize]
+    public class ProfileController : ControllerBase
+    {
+        private readonly IMongoRepository<Profile> _profileRepository;
+        private readonly IUserService _userService;
+
+        public ProfileController(IMongoRepository<Profile> profileRepository,
+            IUserService userService)
+        {
+            _profileRepository = profileRepository;
+            _userService = userService;
+        }
+
+        [HttpGet]
+        public ActionResult<Profile> Get()
+        {
+            try
+            {
+                return Ok(_profileRepository.FindById(ObjectId.Parse(_userService.GetClaimValue(ClaimType.UserId))));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        [HttpPut]
+        public IActionResult Put(ProfilePutDto profilePutDto)
+        {
+            var profile = _profileRepository.FindById(ObjectId.Parse(_userService.GetClaimValue(ClaimType.UserId)));
+
+            if (profile == null)
+                return NotFound(profile.GetType().Name + " not found.");
+
+            profile.Name = profilePutDto.Name ?? profile.Name;
+            profile.Description = profilePutDto.Description ?? profile.Description;
+            profile.UserIdentity = profilePutDto.UserIdentity ?? profile.UserIdentity;
+
+            _profileRepository.ReplaceOne(profile);
+
+            return NoContent();
+        }
+    }
+}
---
+++ AuthenticationApi/ModelsDto/ProfilePutDto.cs
@@ -0,0 +1,9 @@
+﻿namespace AuthenticationApi.ModelsDto
+{
+    public class ProfilePutDto
+    {
+        public string? Name { get; set; } 
+        public string? Description { get; set; }
+        public string? UserIdentity { get; set; } 
+    }
+}
